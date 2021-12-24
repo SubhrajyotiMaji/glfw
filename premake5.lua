0baf45b82a7f96f3c8939afb9cfa415eaeda42af
Subhrajyoti Maji <subhrajyoti.maji@gmail.com>
2021-12-24 18:56:17 +0530

    Updated premake file

--- premake5.lua
+++ premake5.lua
@@ -9,13 +9,24 @@ project "GLFW"
 	{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
+
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
+		"src/internal.h",
+		"src/mappings.h",
 		"src/monitor.c",
+		"src/platform.c",
+		"src/platform.h",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
+
+		"src/null_init.c",
+		"src/null_joystick.c",
+		"src/null_joystick.h",
+		"src/null_monitor.c",
+		"src/null_platform.h",
+		"src/null_window.c"
 	}
 	filter "system:linux"
 		pic "On"
@@ -29,10 +40,10 @@ project "GLFW"
 			"src/x11_monitor.c",
 			"src/x11_window.c",
 			"src/xkb_unicode.c",
-			"src/posix_time.c",
 			"src/posix_thread.c",
-			"src/glx_context.c",
+			"src/posix_time.c",
 			"src/egl_context.c",
+			"src/glx_context.c",
 			"src/osmesa_context.c",
 			"src/linux_joystick.c"
 		}
@@ -50,10 +61,12 @@ project "GLFW"
 		{
 			"src/win32_init.c",
 			"src/win32_joystick.c",
+			"src/win32_module.c",
 			"src/win32_monitor.c",
-			"src/win32_time.c",
 			"src/win32_thread.c",
+			"src/win32_time.c",
 			"src/win32_window.c",
+
 			"src/wgl_context.c",
 			"src/egl_context.c",
 			"src/osmesa_context.c"
